33f8632813f29f2e1a90db643c16edc5df6d5f4f
Leandro Guimaraes <leandroguim@hotmail.com>
2023-10-04 16:04:12 -0300

    Criando Repositorio - Data Access Layer

--- Modelo/Categoria.cs
+++ Modelo/Categoria.cs
@@ -1,7 +1,11 @@
 ﻿namespace Modelo
 {
-    public class Class1
+    public class Categoria
     {
+        private int _id;
+        private string _nome;
 
+        public int Id { get; set; }
+        public string Nome { get; set; }
     }
 }
